5e5a2af1e5c1c9f358cdd2d33dd3085534bfb23e
Johannes <zberzerker246@gmail.com>
2023-05-01 23:34:56 +0800

    Added board randomization

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pygame
+from random import randint
+
 
 
 # Hallowwwwwwwwwwwwwwwww
 
-board = []
+board = [[0 for j in range(9)] for i in range(9)]
 
 # Initializes the board state 
-def initBoard(params, board):
-    pass
+def initBoard(board):
+    for i in range(9):
+        for j in range(9):
+            board[i][j] = randint(0,9)
+
+initBoard(board)
+print(board)
