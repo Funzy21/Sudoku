f10445ed7b94036f2988c94fbf50aba46b85da72
Johannes <zberzerker246@gmail.com>
2023-05-02 01:29:21 +0800

    Added difficulty-based randomization (values still need to be gauged)

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 import pygame
-from random import randint
+from random import randint, choices
 
 
 
-# Hallowwwwwwwwwwwwwwwww
+# Difficulty level can be 1 = Easy, 2 = Medium, 3 = Hard
+def weightedRand(difficulty):
+    values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-board = [[0 for j in range(9)] for i in range(9)]
+    if difficulty == 1:
+        # Easy: 3x more likely for empty tiles to appear
+        weights = [3, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    elif difficulty == 2:
+        # Medium: 3x more likely for empty tiles to appear
+        weights = [5, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    elif difficulty == 3:
+        # Hard: 3x more likely for empty tiles to appear
+        weights = [9, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    else:
+        return None
 
-# Initializes the board state 
-def initBoard(board):
+    return choices(values, weights)[0]
+
+# Initializes the board state based on the given difficulty level
+def initBoard(difficulty):
+    board = [[0 for j in range(9)] for i in range(9)]
+
     for i in range(9):
         for j in range(9):
-            board[i][j] = randint(0,9)
+            board[i][j] = weightedRand(difficulty)
 
-initBoard(board)
-print(board)
+    return board
+
+#testBoard = initBoard(2)
+#print(testBoard)
